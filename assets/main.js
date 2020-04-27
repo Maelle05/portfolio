6a707672f810d6f81d7ddeff16ba4c020723cba2
Maelle05 <maelle.rabouan@gmail.com>
2020-04-27 19:28:14 +0200

    comme origine voir pour responcive

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,32 @@ $('.hamburger-container').click(function(){
 	$('.hamburger-container').toggleClass('active');
 	$('.menu-container').toggleClass('active');
 })
+
+const slider = document.querySelector('.scrollx');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  console.log('aaaa');
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
